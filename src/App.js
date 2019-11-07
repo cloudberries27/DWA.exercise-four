1d8a04b89291d0ed473377199b540d08f6a6f654
cloudberries27 <ccrod2298@gmail.com>
2019-11-07 15:48:24 -0500

    testing

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 var firebase = require('firebase');
+require('firebase/database');
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
