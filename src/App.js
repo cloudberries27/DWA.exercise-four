a542e5f368748754b53977437e1ca187cc58a607
cloudberries27 <ccrod2298@gmail.com>
2019-11-21 09:57:51 -0500

    app.js edit

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ const app =  express();
 const port = process.env.PORT || 4000;
 const path = require('path');
 
-const indexRoute = require ('./routes/index.js');
-const postsRoute =  require ('./routes/posts.js');
-const submitRoute =  require ('./routes/submit.js');
+const indexRoute = require ('../routes/index.js');
+const postsRoute =  require ('../routes/posts.js');
+const submitRoute =  require ('../routes/submit.js');
 
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -21,24 +21,3 @@ app.use("/submit-form", (req,res)=>{
 })
 
 app.listen( port, () => console.log(`Example app listening on port ${port}`));
-import logo from './logo.svg';
-import './App.css';
-var firebase = require('firebase');
-require('firebase/database');
-
-// Your web app's Firebase configuration
-var firebaseConfig = {
-  apiKey: "AIzaSyAxhbNah2ZHxDfGVUSEDSgA1bnc7zqTXiM",
-  authDomain: "anotherone98.firebaseapp.com",
-  databaseURL: "https://anotherone98.firebaseio.com",
-  projectId: "anotherone98",
-  storageBucket: "anotherone98.appspot.com",
-  messagingSenderId: "909298385013",
-  appId: "1:909298385013:web:e07a352a38800a3cba783c"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-var adaRef = firebase.database().ref("blog-posts");
-
-console.log(adaRef);
